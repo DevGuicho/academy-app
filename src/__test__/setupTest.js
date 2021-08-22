02e37cd5477d394ea68ed1dd514e9830ed5b84dc
Luis Vazquez <luvazpa@gmail.com>
2021-08-22 14:12:18 -0500

    Initial config and test environment added

---
+++ src/__test__/setupTest.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+import Enzyme from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import { createSerializer } from 'enzyme-to-json'
+
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }))
+Enzyme.configure({ adapter: new Adapter() })
